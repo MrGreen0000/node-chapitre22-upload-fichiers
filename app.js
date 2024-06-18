f00b7330359b8b20700a0a361df29ce5305efbe2
Naji Mouflih <91195806+MrGreen0000@users.noreply.github.com>
2024-06-18 16:39:55 +0200

    update

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const multer = require("multer");
-const upload = multer({ dest: path.join(__dirname + "/upload") });
+const upload = multer({
+  dest: path.join(__dirname + "/upload"),
+  limits: {
+    fileSize: 10000,
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== "image/jpeg") {
+      return cb(new Error("Seuls les images en JPG sont autorisées"));
+    } else {
+      cb(null, true);
+    }
+  },
+});
 
 const util = require("util");
 require("./database");
@@ -17,27 +29,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { error: null });
 });
 
-app.post("/file", upload.array("avatar", 2), (req, res) => {
-  console.log(
-    util.inspect(req.body, {
-      compact: false,
-      depth: 5,
-      breakLength: 80,
-      color: true,
-    })
-  );
-  console.log(
-    util.inspect(req.files, {
-      compact: false,
-      depth: 5,
-      breakLength: 80,
-      color: true,
-    })
-  );
-  res.end();
+// app.post("/file", upload.single("avatar"), (req, res) => {
+//   console.log(
+//     util.inspect(req.body, {
+//       compact: false,
+//       depth: 5,
+//       breakLength: 80,
+//       color: true,
+//     })
+//   );
+//   console.log(
+//     util.inspect(req.files, {
+//       compact: false,
+//       depth: 5,
+//       breakLength: 80,
+//       color: true,
+//     })
+//   );
+//   res.end();
+// });
+
+app.post("/file", (req, res) => {
+  upload.single("avatar")(req, res, (err) => {
+    if (err) {
+      // En cas d'erreur, renvoyer le message d'erreur à la page de formulaire
+      return res.status(400).render("index", { error: err.message });
+    }
+    res.end();
+  });
 });
 
 app.use((err, req, res, next) => {
@@ -49,7 +71,7 @@ app.use((err, req, res, next) => {
       color: true,
     })
   );
-  req.status(500).redirect("/");
+  res.status(500).redirect("/");
 });
 
 app.listen(3000);
